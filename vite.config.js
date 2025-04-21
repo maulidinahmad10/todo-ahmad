16431f093bceab05d327e89d7ed63c2bb4a5217d
Arwins-code <arwinsyahputra22@gmail.com>
2025-04-21 09:08:36 +0700

    force http vite

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+    base: process.env.APP_URL + '/', // 👈 Add this line
     plugins: [
         laravel({
             input: 'resources/js/app.js',
